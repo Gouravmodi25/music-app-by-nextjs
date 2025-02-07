97063541e9306f47aebd150766787085dbfb25a7
Gouravmodi25 <gouravmodi39@gmail.com>
2025-02-07 13:47:52 +0530

    setup my full stack project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,8 +25,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <h1>NavItem of main layout</h1>
         {children}
       </body>
     </html>
